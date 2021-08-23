5fb66a8dfd68515039147bc23d2b728b7368af49
elcrity <elcrity@naver.com>
2021-08-23 17:21:23 +0900

    0823add Ps

--- chapter08/src/polymorphism/AnimalTest.java
+++ chapter08/src/polymorphism/AnimalTest.java
@@ -12,6 +12,9 @@ class Human extends Animal{
 	public void move() {
 		System.out.println("사람이 두발로 걷습니다.");
 	}
+	public void sleep() {
+		System.out.println("사람이 잠을 잡니다.");
+	}
 	
 	public void readbook() {
 		System.out.println("사람이 책을 읽습니다.");
@@ -21,6 +24,9 @@ class Tiger extends Animal{
 	public void move() {
 		System.out.println("호랑이가 네발로 걷습니다.");
 	}
+	public void sleep() {
+		System.out.println("호랑이가 잠을 잡니다.");
+	}
 	public void huting() {
 		System.out.println("호랑이가 사냥을 합니다.");
 	}
@@ -29,6 +35,10 @@ class Eagle extends Animal{
 	public void move() {
 		System.out.println("독수리가 하늘을 납니다.");
 	}
+	public void slep() {
+		System.out.println("독수리가 잠을 잡니다.");
+	}
+	
 	public void flying() {
 		System.out.println("독수리가 날개를 펴고 하늘을 납니다.");
 	}
@@ -51,7 +61,9 @@ public class AnimalTest{
 		
 		for(Animal ani : aniList){
 			ani.move();
+			//ani.sleep : animal인스턴스에는 sleep 메소드가 없어서 컴파일 오류
 		}
+		
 	}
 	
 	public void testCasting() {
@@ -60,12 +72,15 @@ public class AnimalTest{
 			if(ani instanceof Human) { // Human -> 변경 : 지원되지 않는 형 출력, 오류 출력
 				Human h = (Human)ani; // Human -> 변경 : 컴파일 에러
 				h.readbook();
+				h.sleep();
 			}else if(ani instanceof Tiger) {
 				Tiger t = (Tiger)ani;
 				t.huting();
+				t.sleep();
 			}else if(ani instanceof Eagle) {
 			Eagle e = (Eagle)ani;
-			e.flying();
+				e.flying();
+				e.slep();
 			}
 			else {
 				System.out.println("지원되지 않는 형");
