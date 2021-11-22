4926afc85dd5f29585bf45f56b250a9dc9fd50f6
elcrity <elcrity@naver.com>
2021-11-22 18:32:35 +0900

    1122오류 발생한 메소드 주석처리

--- programers/src/level1/SumTwoNum_HashSet.java
+++ programers/src/level1/SumTwoNum_HashSet.java
@@ -4,33 +4,31 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.Arrays;
 
-class SumNum_HashSet{
-	public static String solution(int[] numbers) {
-        Set<Integer> set = new HashSet<>();
-
-        for(int i=0; i<numbers.length; i++) {
-            for(int j=i+1; j<numbers.length; j++) {
-                set.add(numbers[i] + numbers[j]);
-            }
-        }
-        return set.toString();
-        //parseInt는 static, String형 객체에서 int를 가져옴 String -> int
-	}
-}
-
-class SumNum_HashSet_Integer{
-	public static Set<Integer> solution(int[] numbers) {
-        Set<Integer> set = new HashSet<>();
-
-        for(int i=0; i<numbers.length; i++) {
-            for(int j=i+1; j<numbers.length; j++) {
-                set.add(numbers[i] + numbers[j]);
-            }
-        }
-        return set;
-        //parseInt는 static, String형 객체에서 int를 가져옴 String -> int
-	}
-}
+//class SumNum_HashSet{//"[배열]"로 출력, 오류
+//	public static String solution(int[] numbers) {
+//        Set<Integer> set = new HashSet<>();
+//
+//        for(int i=0; i<numbers.length; i++) {
+//            for(int j=i+1; j<numbers.length; j++) {
+//                set.add(numbers[i] + numbers[j]);
+//            }
+//        }
+//        return set.toString();
+//	}
+//}
+//
+//class SumNum_HashSet_Integer{//오류 발생
+//	public static Set<Integer> solution(int[] numbers) {
+//        Set<Integer> set = new HashSet<>();
+//
+//        for(int i=0; i<numbers.length; i++) {
+//            for(int j=i+1; j<numbers.length; j++) {
+//                set.add(numbers[i] + numbers[j]);
+//            }
+//        }
+//        return set;
+//	}
+//}
 
 class SumNum_HashSet_Stream{
 	public static int[] solution(int[] numbers) {
@@ -56,8 +54,8 @@ public class SumTwoNum_HashSet {
 	public static void main(String[] args) {
 		int[] numbers = {2,1,3,4,1};
 		System.out.println("ArrayList : "+Arrays.toString(SumNum_HashSet_Stream.solution(numbers)));
-		System.out.println("ArrayList : "+SumNum_HashSet_Integer.solution(numbers));
-		System.out.println("ArrayList : "+SumNum_HashSet.solution(numbers));
+//		System.out.println("ArrayList : "+SumNum_HashSet_Integer.solution(numbers));
+//		System.out.println("ArrayList : "+SumNum_HashSet.solution(numbers));
 	}
 
 }
